ca3d8425fd9f921babb000b9471cdeb433054f55
Rankaisija <65511061+turpaan64@users.noreply.github.com>
2022-01-26 13:59:50 +0200

    `0.1.5.3`

--- z64rom.c
+++ z64rom.c
@@ -1,6 +1,6 @@
 #include "lib/z64rom.h"
 
-char* sToolName = PRNT_PRPL "z64rom " PRNT_GRAY "0.1.5.2"
+char* sToolName = PRNT_PRPL "z64rom " PRNT_GRAY "0.1.5.3"
 #ifndef NDEBUG
 		PRNT_DGRY " DEBUG BUILD"
 #endif

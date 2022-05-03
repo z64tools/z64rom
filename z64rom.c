a5ac63ccd10bbb36811f1236a82d9c815356e7a9
rankaisija <n64rankaisija@gmail.com>
2022-05-04 00:02:31 +0300

    Update z64rom.c

--- z64rom.c
+++ z64rom.c
@@ -9,7 +9,7 @@
 
  */
 
-const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.5.1"
+const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.5.2"
 #ifndef NDEBUG
 	PRNT_DGRY " DEBUG BUILD"
 #endif

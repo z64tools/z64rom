5f35d770c5d7f976af7d9898bb41ac7b43eb1575
rankaisija <n64rankaisija@gmail.com>
2022-05-06 01:07:42 +0300

    Ver Adv

--- z64rom.c
+++ z64rom.c
@@ -9,7 +9,7 @@
 
  */
 
-const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.5.4"
+const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.5.5"
 #ifndef NDEBUG
 	PRNT_DGRY " DEBUG BUILD"
 #endif

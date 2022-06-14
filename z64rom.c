f3c0e103ca876d9e56bec46bd22315832f507ebf
rankaisija <n64rankaisija@gmail.com>
2022-06-15 02:14:59 +0300

    Update z64rom.c

--- z64rom.c
+++ z64rom.c
@@ -3,7 +3,7 @@
 #include "src/Package.h"
 #include <xm.h>
 
-const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.9.4";
+const char* gToolName = PRNT_BLUE "z64rom " PRNT_GRAY "0.9.5";
 
 s32 gDumpRom = -1;
 s32 gDumpAudio = -1;
